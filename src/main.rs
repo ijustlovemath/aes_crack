9e7b730c6c6a3bbbc17cf4995802f3d0a961f725
Jeremy DeJournett <jcdejournett@gmail.com>
2024-03-11 16:45:23 -0400

    automate corruptoion, added more messages about ciphertext and solved plaintext, removed unnecessary clones and stop channels

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,8 @@ struct CrackContext {
     indices: Vec<usize>,
     partial_key: GenericArray<u8, U16>,
     message: GenericArray<u8, U16>,
-    solution: Vec<u8>,
-    soln_tx: Sender<(usize, Vec<u8>)>,
+    solution: GenericArray<u8, U16>,
+    soln_tx: Sender<(usize, GenericArray<u8, U16>)>,
   //  stop_rx: Receiver<()>,
 }
 
@@ -32,16 +32,18 @@ fn main() {
 
     cipher.encrypt_block(&mut message);
 
-    // mess up the key so we have to fix it
-    partial_key[0] = 255;
-    partial_key[1] = 73;
-    partial_key[5] = 72;
-
-    // indices we know are broken
+    // indices we want to break
     let mut corrupted = Vec::new();
     corrupted.push(0);
     corrupted.push(1);
     corrupted.push(5);
+    corrupted.push(6);
+    //corrupted.push(11);
+
+    // automatically mess up the key so we have to fix it
+    for i in &corrupted {
+        partial_key[*i] = 0;
+    }
 
 
     let WORKERS = 200;
@@ -55,8 +57,8 @@ fn main() {
             id: i,
             indices: corrupted.clone(),
             partial_key: partial_key,
-            message: message,
-            solution: solution.to_vec(),
+            message: message.clone(),
+            solution: solution,
             soln_tx: soln_tx.clone(),
         };
 
@@ -67,22 +69,22 @@ fn main() {
             let mut rng = rand::thread_rng();
 
             loop {
-                let mut last_j = 0;
+                //let mut last_j = 0;
                 for j in &work.indices {
                     work.partial_key[*j] = rng.gen();
-                    last_j += j;
+                    //last_j += j;
                 }
 //                if last_j == 0 {
 //                    if stop_rx.try_recv().is_ok() {
 //                        break;
 //                    }
 //                }
+                // you need to clone this so you don't mess up the ciphertext every iteration
                 let mut workspace = work.message.clone();
-                let key = work.partial_key.clone();
-                let cipher = Aes128::new(&key);
+                let cipher = Aes128::new(&work.partial_key);
                 cipher.decrypt_block(&mut workspace);
-                if workspace.to_vec() == work.solution {
-                    work.soln_tx.send((work.id, work.partial_key.to_vec()));
+                if workspace == work.solution {
+                    work.soln_tx.send((work.id, work.partial_key));
                 }
 
 
@@ -94,5 +96,13 @@ fn main() {
 //    for stop in stop_channels {
 //        stop.send(()).expect("failed to stop everyone");
 //    }
-    println!("solved by {solver}: {solved_key:#?}");
+    let printable_key = solved_key.to_vec();
+    println!("solved by {solver}: key={printable_key:?}");
+    let mut decrypted = message.clone();
+    let decryptor = Aes128::new(&solved_key);
+    let ciphertext = decrypted.to_vec();
+    println!("ciphertext: {ciphertext:?}");
+    decryptor.decrypt_block(&mut decrypted);
+    let plaintext = decrypted.to_vec();
+    println!("plaintext: {plaintext:?}");
 }
