36d9d8179ef4903396ad49da4a45a29049e8e81b
Jeremy DeJournett <jeremy@idealmedtech.com>
2024-03-11 14:26:38 -0400

    now supports solver id and stopping prematurely

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,12 @@ use aes::cipher::{
 
 #[derive(Clone)]
 struct CrackContext {
+    id: usize,
     indices: Vec<usize>,
     partial_key: GenericArray<u8, U16>,
     message: GenericArray<u8, U16>,
     solution: Vec<u8>,
-    soln_tx: Sender<Vec<u8>>,
+    soln_tx: Sender<(usize, Vec<u8>)>,
   //  stop_rx: Receiver<()>,
 }
 
@@ -34,47 +35,64 @@ fn main() {
     // mess up the key so we have to fix it
     partial_key[0] = 255;
     partial_key[1] = 73;
+    partial_key[5] = 72;
 
     // indices we know are broken
     let mut corrupted = Vec::new();
     corrupted.push(0);
     corrupted.push(1);
+    corrupted.push(5);
 
 
     let WORKERS = 20;
     let (soln_tx, soln_rx) = channel();
-    //let (stop_tx, stop_rx) = channel();
+    let mut stop_channels = Vec::new();
 
     for i in 0..WORKERS {
+
+        let (stop_tx, stop_rx) = channel();
         let work = CrackContext {
+            id: i,
             indices: corrupted.clone(),
             partial_key: partial_key,
             message: message,
             solution: solution.to_vec(),
-    //        stop_rx: stop_rx.clone(),
             soln_tx: soln_tx.clone(),
         };
+
+        stop_channels.push(stop_tx);
+
         thread::spawn(move || {
             let mut work = work.clone();
             let mut rng = rand::thread_rng();
+            let mut last_j = 0;
 
             loop {
                 for j in &work.indices {
                     work.partial_key[*j] = rng.gen();
+                    last_j += j;
+                }
+                if last_j == 0 {
+                    if stop_rx.try_recv().is_ok() {
+                        break;
+                    }
                 }
                 let mut workspace = work.message.clone();
                 let key = work.partial_key.clone();
                 let cipher = Aes128::new(&key);
                 cipher.decrypt_block(&mut workspace);
                 if workspace.to_vec() == work.solution {
-                    work.soln_tx.send(work.partial_key.to_vec());
-                    break;
+                    work.soln_tx.send((work.id, work.partial_key.to_vec()));
                 }
+
 
             }
         });
 
     }
-    let solved_key = soln_rx.recv().unwrap();
-    println!("solved: {solved_key:#?}");
+    let (solver, solved_key) = soln_rx.recv().unwrap();
+    for stop in stop_channels {
+        stop.send(()).expect("failed to stop everyone");
+    }
+    println!("solved by {solver}: {solved_key:#?}");
 }
